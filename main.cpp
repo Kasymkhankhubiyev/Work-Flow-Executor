76efccfeb1f420fc4a9e614d65984445aa510e24
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2019-12-22 16:40:05 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,11 @@
 
 using namespace std;
 
-void get_next_command(std::string line) //index, name, arg
-{
 
-}
 
 int main()
 {
+	size_t*arr = new size_t[100]; 
 	std::map<std::string, Worker*> inst; //Workers storage
 	inst["Read"] = new Read();
 	inst["Write"] = new Write();
@@ -21,13 +19,14 @@ int main()
 	inst["Dump"] = new Dump();
 
 	std::vector<std::string>method;
+	std::vector<std::string>argument;
 
-	std::string * resource = new string; 
+	//std::string * resource = new string; 
 
-	std::map<size_t,pair<Worker*, vector<string>>>programm;
+	std::map<size_t,pair<Worker*,vector<string>>>programm;
 
-	 //создаем класс, позволяющий записывать данные в файл;
-	 
+	//создаем класс, позволяющий записывать данные в файл;
+	int k = 0;
 	std::string path = "Command.txt";
 	Parser parser(path);
 	parser.OpenFile(); //open может как открыть имеющийся файл по указанному маршруту, так и создать новый, так myFile просто появится у нас.
@@ -40,25 +39,48 @@ int main()
 		{
 			std::string str;
 			str = parser.get_line();
-			method.push_back(str);
+			if (str != "csed")
+			{
+				size_t q = parser.get_com_num(str);
+				std::string trip = parser.get_com_name(str);
+				method.push_back(trip);
+				if(inst[trip] == nullptr)
+				{
+					//compare_df(inst, trip);
+					//нужно создать новый элемент, иначе пропустить все остальное, вообще, например, сортировку можно вызвать несколько раз, сначала один аргумент,
+					//потом другой, не совсем понятно. когда ставить сравнение, с другой стороны, мы же не будем несколько раз куда-то указывать, думаю,
+				// вполне достточно проверить самый простой случай.
+				}
+				//parser.get_com_args(str,argument); //вообще можно не разделять аргументы
+				programm[q] = pair<Worker*,vector<string>>(inst[trip], str);
+				//programm[0].first //worker*
+				//programm[0].second //argument
+			}
+			else
+			{
+				while (!parser.endOfInstruction())
+				{
+					std::string trin;
+					trin = parser.get_line();
+					size_t n, num, ptr;
+					while (trin.size() >= 1 )  //std::string::npos
+					{						
+						arr[k] = parser.get_com_arr(trin);
+						k++;
+						//else trin = trin.substr(0);
+					}					
+				}
+			}
 			i++;
 		}
 	}
 	parser.CloseFile();
-
-	int vector_size = method.size();
-	for (int j = 0; j < vector_size; j++)
-	{
-		std::string command = method[j]; // get command line
-		// реализуем метод считвания символов строки"
-		
-	}
-
-	cout << "Now, let's print out the vector";
-	
-	for (int k = 0; k < vector_size; k++)
-	{
-		cout << method[k] << '\n';
-	}
+	//теперь нужно все распихать по контейнерам.
+	delete inst["Read"];
+	delete inst["Write"];
+	delete inst["Sort"];
+	delete inst["Grep"];
+	delete inst["Replace"];
+	delete inst["Dump"];
 	cout << "Good bye!";
 }
