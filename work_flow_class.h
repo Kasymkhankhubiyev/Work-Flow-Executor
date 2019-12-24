d8775b1ff2d1c54eb387ed952b6620c5f8a375dc
Kasymkhankhubiyev <55043545+Kasymkhankhubiyev@users.noreply.github.com>
2019-12-24 16:29:22 +0700

    Update work_flow_class.h

--- work_flow_class.h
+++ work_flow_class.h
@@ -17,17 +17,13 @@ class Parser //анализатор текстовой информации;
 {
 private:
 	std::string m_parser;
-	std::ifstream m_fin;
-	size_t m_ind;
+	std::ifstream m_fin; //для чтения файла
+	size_t m_ind; //для сохранения кол-ва выполняемы действий
 	std::string m_com;
 	std::string m_arg;
 public:
-	Parser(std::string link);
-	/*string get_line()
-	{
-		std::string str;
-		return getline(m_fin, str);
-	}*/
+	Parser(std::string link); //конструктор
+
 	std::string get_link();
 
 	std::string get_line();
@@ -35,8 +31,6 @@ public:
 	void set_numbers(size_t& a);
 
 	size_t Get_numbers();
-
-	void get_next_comm(std::string line);
 
 	void OpenFile();
 
@@ -57,41 +51,42 @@ public:
 };
 class Validator
 {};
-class Worker
-{
-	//static std::string* resource;
+class Worker //релизуем через наследование.
+{ 
+	
+	
 	std::string m_str;
 protected:
 	static std::vector<std::string> m_vect;	
 	static int my_ind;
 public:
-	
+	void initData(std::string source);
 	virtual void TO_DO(std::string& str) = 0;
 };
 class Read : public Worker
 {
-	int m_Read_ind;
+	size_t m_index;
 	std::ifstream m_fin;
 public:
-	Read();
+	Read()
+	{
+		m_index = 0;
+	}
 	void TO_DO(std::string& str);
 };
 class Write : public Worker
 {
-	std::ofstream m_fin1;
-	int m_Write_ind;
+	std::ofstream m_fin1; //чтобы записывать
 public:
 	void TO_DO(std::string& str);
 };
 class Sort : public Worker
 {
-	int m_Sort_ind;
 public:
 	void TO_DO(std::string& str);
 };
 class Grep : public Worker
 {
-	int m_Grep_ind;
 public:
 	void TO_DO(std::string& str);
 };
@@ -101,13 +96,15 @@ class Replace : public Worker
 public:
 	void TO_DO(std::string& str);
 };
-class Dump : public Worker
+/*class Dump : public Worker
 {
 	int m_Dump_ind;
 public:
+	Dimp() {}
 	void TO_DO(std::string& str);
-};
-class MyError : std::exception {
+};*/
+class MyError : std::exception
+{
 	const char* what() const;
 };
 #endif//
